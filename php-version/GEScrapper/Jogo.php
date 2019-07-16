1cecea775415fd0cd8c0a077435c746d8407f34e
Anderson Souza <andersom.eugenio@gmail.com>
2019-07-16 01:07:52 -0300

    comecei a modelar e troquei a lib de parse

---
+++ php-version/GEScrapper/Jogo.php
@@ -0,0 +1,9 @@
+<?php
+namespace GEScrapper;
+
+class Jogo {
+    private $time_casa, $time_visitante, $horario, $competicao, $estado, $gols_casa, $gols_visitante;
+    function __construct(Time $time_casa, Time $time_visitante){
+
+    }
+}
--- php-version/get-jogos.php
+++ php-version/get-jogos.php
@@ -1,17 +1,15 @@
 <?php
 require_once("vendor/autoload.php");
 use PHPHtmlParser\Dom;
-
-$dom = new Dom;
-$dom->loadFromUrl('https://globoesporte.globo.com/placar-ge/hoje/jogos.ghtml');
-$jogos = $dom->find('.resultado-time-container');
+$html = Pharse::file_get_dom('https://globoesporte.globo.com/placar-ge/hoje/jogos.ghtml'); 
+$jogos = $html('.resultado-time-container');
 foreach($jogos as $jogo){
-    $times = $jogo->find('.nome-completo');
-    $placar = $jogo->find('.placar');
+    $times = $jogo('.nome-completo');
+    $placar = $jogo('.placar');
     if(count($placar)>1){
-        echo(" {$times[0]->text} {$placar[0]->text} x {$placar[1]->text} {$times[1]->text} \n");
+        echo(" {$times[0]->getPlainText()} {$placar[0]->getPlainText()} x {$placar[1]->getPlainText()} {$times[1]->getPlainText()} \n");
     }else{
-        echo(" {$times[0]->text} x {$times[1]->text} \n");
+        echo(" {$times[0]->getPlainText()} x {$times[1]->getPlainText()} \n");
 
     }
-}
+} 
